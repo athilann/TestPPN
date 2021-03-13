defa5a7f914b289c55879f922469453bcc675d5c
Athila Ninow <athila.ninow@leacam.ie>
2021-03-13 03:00:41 +0000

    Add project files.

---
+++ TesteTeoria/Particula.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteTeoria
+{
+    public class Particula
+    {
+        private decimal _v = 0;  // Backing store
+        private decimal _m = 0;  // Backing store
+        private decimal _e = 0;  // Backing store
+        private decimal _g = 0;  // Backing store
+
+        public decimal V { get => _m + _v; set { _v += value; } }
+        public decimal M { get => _v - _g; set { _m += value; } }
+        public decimal E { get => _v + _m; set { _e = value; } }
+        public decimal G { get => _m + _e; set { _g = value; } }
+
+        public Particula(int v, int m, int e, int g)
+        {
+            V = v;
+            M = M;
+            E = E;
+            G = G;
+        }
+
+        public void Run()
+        {
+            M = M;
+            V = V;
+            E = E;
+            G = G;
+        }
+
+
+    }
+}
---
+++ TesteTeoria/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TesteTeoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var particula = new Particula(5, 0, 0, 0);
+            Console.WriteLine("----Inicio----");
+
+
+            Console.WriteLine("V: " + particula.V);
+            Console.WriteLine("M: " + particula.M);
+            Console.WriteLine("E: " + particula.E);
+            Console.WriteLine("G: " + particula.G);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            var count = 1;
+            while (particula.M != 0)
+            {
+                Console.WriteLine("----Clico " + count + " ----");
+                particula.Run();
+                Console.WriteLine("V: " + particula.V);
+                Console.WriteLine("M: " + particula.M);
+                Console.WriteLine("E: " + particula.E);
+                Console.WriteLine("G: " + particula.G);
+                Console.WriteLine("-----------------");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                count++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
