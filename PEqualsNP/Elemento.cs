472d7e43d937be1ea35289f13394eeffdb343bf0
Athila Ninow <athila.ninow@leacam.ie>
2021-03-15 12:37:35 +0000

    carculo de particula estavel

--- PEqualsNP/Elemento.cs
+++ PEqualsNP/Elemento.cs
@@ -11,8 +11,8 @@ namespace PEqualsNP
         public int Neutrons { get; set; }
         public int Protons { get; set; }
         public int Eletrons { get; set; }
-        public double A { get => ((Eletrons * 0.5 + Neutrons) / Neutrons * 3); }
-        public double Carga { get { return (A * (A + 0.5)); } }
+        public double A { get => ((Eletrons * Espaco + Neutrons) / Neutrons * 3); }
+        public double Carga { get { return (A * (A + Espaco)); } }
         public double Espaco { get { return (Entropia * 0.5); } }
         public double EstadoXYZ
         {
@@ -28,7 +28,7 @@ namespace PEqualsNP
         {
             get
             {
-                double EspacoEletrico = (Protons - (Espaco - EstadoXYZ));
+                double EspacoEletrico = (Protons - (Espaco - EstadoXYZ)) / A * 3;
                 double MomentoEletrico = ((Eletrons - (Espaco - EstadoXYZ)) / A * 3);
                 double SinalEletrico = ((Neutrons - (Espaco - EstadoXYZ)) / A * 3);
                 return ((EspacoEletrico + MomentoEletrico + SinalEletrico) * A) / A * 3;
@@ -39,22 +39,32 @@ namespace PEqualsNP
         {
             get
             {
-                return (EstadoEletrico - CargaNeutra) * A / 3 * A;
+                var estadoEletrico = ((Protons + Eletrons) * Carga) / EstadoEletrico;
+                var estadoNeutro = ((Neutrons * Carga) / EstadoEnergetico) / Espaco;
+                return estadoEletrico + estadoNeutro;
             }
         }
         public double CargaNeutra
         {
             get
             {
-                return (EstadoEletrico + EstadoXYZ) * A / 3 * A;
+                return (EstadoEnergetico * EstadoXYZ) / A * (3 * A);
             }
         }
 
-        public double EstadoNeutro
+        public double CargaEletrica
         {
             get
             {
-                return 0;
+                return (EstadoEletrico / EstadoXYZ) * A / 3 * A;
+            }
+        }
+
+        public double EstadoEnergetico
+        {
+            get
+            {
+                return (Carga * Entropia) / Entropia * 3;
             }
         }
     }
